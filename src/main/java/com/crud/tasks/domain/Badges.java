419d4015f03f28d1bfcbeb4d47a9560c317cecdd
Maciej Nycz <nycz.maciej91@gmail.com>
2018-12-12 16:39:25 +0000

    18.3

---
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Badges {
+
+    @JsonProperty("votes")
+    private Integer votes;
+
+    @JsonProperty("attachmentyByType")
+    private TrelloAttachmentByType attachmentByType;
+}
