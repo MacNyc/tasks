38b1200fc182dc4a346c5f805332e6fc2e20c912
Maciej Nycz <nycz.maciej91@gmail.com>
2018-12-14 20:33:56 +0000

    better

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -11,10 +11,10 @@ public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
+}
 
 	//@Override
 	//protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
 		//return application.sources(TasksApplication.class);
 
-	 //}
-}
+	// }
