9814e5f85d68bf3664c1a091e134ef252b76ba5d
Maciej Nycz <nycz.maciej91@gmail.com>
2018-12-16 14:17:40 +0000

    email

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,7 +22,7 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-    @Scheduled(cron = "0 0 12 * * * ")
+    @Scheduled(cron = "0 0 10 * * * ")
     public  void sendInformationEmail(){
         long size = taskRepository.count();
         simpleEmailService.send(new Mail(
